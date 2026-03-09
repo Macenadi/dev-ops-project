ad4e9fa33165a1c24aceac7720c73231c091a9cb
Nádia Cardoso de Macedo <nadia_cardoso_macedo@hotmail.com>
2026-03-09 15:36:06 +0000

    project

---
+++ dev-ops-project/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using dev_ops_project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dev_ops_project.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
+
--- dev-ops-project/Program.cs
+++ dev-ops-project/Program.cs
@@ -1,7 +1,13 @@
+using dev_ops_project.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer
+(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -15,15 +21,11 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
-
 app.Run();
+
