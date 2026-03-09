dcd7594be7b18e74b7ac8bda276df13f305ea894
Nádia Cardoso de Macedo <nadia_cardoso_macedo@hotmail.com>
2026-03-09 16:34:38 +0000

    Last update

--- dev-ops-project/Program.cs
+++ dev-ops-project/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Console.WriteLine(builder.Configuration.GetConnectionString("DefaultConnection"));
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
